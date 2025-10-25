73b15eb30748479e130b26e3930206af4c4bb8a4
jonathabsb-alt <jonatha.bsb@gmail.com>
2025-10-25 15:07:01 -0300

    feat: Cria arquivo script.js e linka em todas as páginas para funcionalidades dinâmicas

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+console.log("Script.js carregado com sucesso! Iniciando funcionalidades dinâmicas...");
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM totalmente carregado e pronto para a interação.");
+});
